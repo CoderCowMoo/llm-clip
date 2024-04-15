9de9f04691f9549765c8748141a79ed1b67f67ae
CoderCowMoo <broskichad38@gmail.com>
2024-04-15 15:04:54 +1000

    Fix tests

--- tests/test_clip.py
+++ tests/test_clip.py
@@ -1,13 +1,13 @@
 import llm
 
 
-def clip_test_run_embedding():
+def test_clip_run_embedding():
     model = llm.get_embedding_model("clip")
     result = model.embed("bunny")
     assert len(result) == 512
     assert isinstance(result[0], float)
 
-def siglip_test_run_embedding():
+def test_siglip_run_embedding():
     model = llm.get_embedding_model("siglip")
     result = model.embed("bunny")
     assert len(result) == 1152
